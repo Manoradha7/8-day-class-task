e22042bb6641826ca47f4c358c882c41562de921
Manoradha7 <91074732+Manoradha7@users.noreply.github.com>
2021-09-23 21:15:59 +0530

    Create acc.js

---
+++ acc.js
@@ -0,0 +1,30 @@
+class account{
+    constructor(name, accno, balance)
+    {
+        this.name=name;
+        this.accno=accno;
+        this.balance=balance;
+    }
+    getBalance(){
+        return "The Balance is :"+this.balance
+    }
+    withdraw(amount){
+        if(amount<=this.balance)
+        {
+            this.balance=this.balance-amount
+            return this.getBalance()
+        }else{
+            return "The Invalid amount.the balance is:"+this.getBalance()
+        }
+    }
+    deposit(amount){
+        this.balance=this.balance+amount
+        return this.getBalance()
+    }
+}
+const Mega=new account("Mano",5746,234579)
+const Arasan=new account("Radha",2314,342560)
+const Mano=new account("Veera",3245,343500)
+console.log(Radha.getBalance());
+console.log(Mano.deposit(500000));
+console.log(Veera.withdraw(50000));
